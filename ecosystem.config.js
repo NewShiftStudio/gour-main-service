f47eca778976ccb352493dbf9540d1b8e21b0ca5
zolkinka <32388803+zolkinka@users.noreply.github.com>
2022-03-30 17:55:14 +0300

    добавил ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+const pack = require('./package.json');
+
+module.exports = {
+  apps: [
+    {
+      name: `${pack.name}`,
+      script: 'dist/index.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '100M',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
