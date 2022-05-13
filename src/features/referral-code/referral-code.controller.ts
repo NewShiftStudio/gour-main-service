eb6351b3f27b0543854cb43895aeb4447fa255aa
zolkinka <32388803+zolkinka@users.noreply.github.com>
2022-05-13 11:16:42 +0400

    добавил выгрузку xlsx файла экспорта по реферальным кодам

--- src/features/referral-code/referral-code.controller.ts
+++ src/features/referral-code/referral-code.controller.ts
@@ -8,6 +8,7 @@ import {
   Post,
   Put,
   Query,
+  Res,
 } from '@nestjs/common';
 import { ReferralCodeCreateDto } from './dto/ReferralCodeCreateDto';
 import { ReferralCodeService } from './referral-code.service';
@@ -15,6 +16,10 @@ import { BaseGetListDto } from '../../common/dto/BaseGetListDto';
 import { ApiTags } from '@nestjs/swagger';
 import { ReferralCodeExportDto } from './dto/referral-code.export.dto';
 import { ReferralCodeSetDiscountDto } from './dto/referral-code.set-discount.dto';
+import * as XLSX from 'xlsx';
+import { Response } from 'express';
+import { Readable } from 'stream';
+import { Client } from '../../entity/Client';
 
 @ApiTags('referralCodes')
 @Controller()
@@ -42,8 +47,19 @@ export class ReferralCodeController {
   }
 
   @Get('/referralCodes/export')
-  export(@Query() params: ReferralCodeExportDto) {
-    return this.referralCodeService.getClientStat(params);
+  async export(@Query() params: ReferralCodeExportDto, @Res() res: Response) {
+    const clients = await this.referralCodeService.getClientStat(params);
+
+    const wb = this.makeBook(clients);
+    const fileName = `referral_codes_export_${new Date().toISOString()}`;
+    res.set({
+      'Content-Type':
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      'Content-Disposition': `attachment; filename="${fileName}.xlsx"`,
+    });
+    return res
+      .status(200)
+      .send(XLSX.write(wb, { type: 'buffer', bookType: 'xlsx' }));
   }
 
   @Get('/referralCodes/discount')
@@ -54,5 +70,19 @@ export class ReferralCodeController {
   @Post('/referralCodes/discount')
   setDiscount(@Body() { discount }: ReferralCodeSetDiscountDto) {
     return this.referralCodeService.setDiscount(discount);
+  }
+
+  makeBook(clients: Client[]) {
+    const ws = XLSX.utils.aoa_to_sheet([
+      ['Code', 'Client Name', 'Date'],
+      ...clients.map((client) => [
+        client.referralCode.code,
+        client.name,
+        client.createdAt,
+      ]),
+    ]);
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'SheetJS');
+    return wb;
   }
 }
