9843add508d69f34ba6977d4dd4a6279a4a63859
Artyom Vorokhta <avorohta@newshift.ru>
2022-07-19 17:43:23 +0300

    product microservice refactoring

--- src/features/product/product.controller.ts
+++ src/features/product/product.controller.ts
@@ -1,27 +1,16 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Post,
-  Put,
-  Query,
-  Res,
-} from '@nestjs/common';
-import { ProductGetListDto } from './dto/product.get-list.dto';
-import { ProductCreateDto } from './dto/product.create.dto';
+import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { ApiTags } from '@nestjs/swagger';
+
+import { ProductGetListDto } from './dto/product-get-list.dto';
+import { ProductCreateDto } from './dto/product-create.dto';
 import { ProductService } from './product.service';
-import { ProductGetOneDto } from './dto/product.get-one.dto';
+import { ProductGetOneDto } from './dto/product-get-one.dto';
 import { ProductGradeService } from './product-grade.service';
-import { ProductGradeCreateDto } from './dto/product-grade.create.dto';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
-import { ProductUpdateDto } from './dto/product.update.dto';
-import { TOTAL_COUNT_HEADER } from '../../constants/httpConstants';
-import { Response } from 'express';
-import { ProductGradeGetListDto } from './dto/product-grade.get-list.dto';
-import { ProductGradeUpdateDto } from './dto/product-grade.update.dto';
-import { ProductWithMetricsDto } from './dto/product-with-metrics.dto';
+import { ProductGradeCreateDto } from './dto/product-grade-create.dto';
+import { ProductUpdateDto } from './dto/product-update.dto';
+import { ProductGradeGetListDto } from './dto/product-grade-get-list.dto';
+import { ProductGradeUpdateDto } from './dto/product-grade-update.dto';
 
 @ApiTags('products')
 @Controller()
@@ -31,68 +20,65 @@ export class ProductController {
     private readonly productGradeService: ProductGradeService,
   ) {}
 
-  @Get('/products')
-  async getAll(@Query() params: ProductGetListDto, @Res() res: Response) {
-    const [products, count] = await this.productService.findMany(params);
-
-    res.set(TOTAL_COUNT_HEADER, count.toString());
-    return res.send(products);
+  @MessagePattern('get-products')
+  getAll(@Payload() params: ProductGetListDto) {
+    return this.productService.findMany(params);
   }
 
-  @Get('/products/novelties')
-  getNovelties(@Query() params: ProductGetListDto) {
+  @MessagePattern('get-novelties')
+  getNovelties(@Payload() params: ProductGetListDto) {
     return this.productService.findNovelties(params);
   }
 
-  @ApiResponse({
-    type: ProductWithMetricsDto,
-  })
-  @Get('/products/:id')
-  getOne(@Param('id') id: string, @Query() params: ProductGetOneDto = {}) {
-    return this.productService.getOne(+id, params);
+  @MessagePattern('get-product')
+  getOne(
+    @Payload('id') id: number,
+    @Payload('params') params: ProductGetOneDto = {},
+  ) {
+    return this.productService.getOne(id, params);
   }
 
-  @Post('/products')
-  async post(@Body() product: ProductCreateDto) {
-    return this.productService.create(product);
+  @MessagePattern('create-product')
+  post(@Payload() dto: ProductCreateDto) {
+    return this.productService.create(dto);
   }
 
-  @Put('/products/:id')
-  put(@Param('id') id: string, @Body() product: ProductUpdateDto) {
-    return this.productService.update(+id, product);
+  @MessagePattern('edit-product')
+  put(@Payload('id') id: number, @Payload('dto') dto: ProductUpdateDto) {
+    return this.productService.update(id, dto);
   }
 
-  @Delete('/products/:id')
-  remove(@Param('id') id: string, @Query('hard') hard: boolean) {
-    return this.productService.remove(+id, !!hard);
+  @MessagePattern('delete-product')
+  remove(@Payload('id') id: number, @Payload('hard') hard: boolean) {
+    return this.productService.remove(id, hard);
   }
 
-  @Get('/products/:productId/grades')
+  @MessagePattern('get-product-grades')
   getProductGrades(
-    @Param('productId') productId: string,
-    @Query() params: ProductGradeGetListDto,
+    @Payload('id') id: number,
+    @Payload('params') params: ProductGradeGetListDto,
   ) {
-    return this.productGradeService.findFromProduct(+productId, params);
+    return this.productGradeService.findFromProduct(id, params);
   }
 
-  @Post('/products/:productId/grades')
+  @MessagePattern('create-product-grade')
   createProductGrades(
-    @Param('productId') productId: string,
-    @Body() grade: ProductGradeCreateDto,
+    @Payload('id') id: number,
+    @Payload('dto') dto: ProductGradeCreateDto,
   ) {
-    return this.productGradeService.create(+productId, grade);
+    return this.productGradeService.create(id, dto);
   }
 
-  @Get('/productGrades')
-  getGrades(@Query() params: ProductGradeGetListDto) {
+  @MessagePattern('get-grades')
+  getGrades(@Payload() params: ProductGradeGetListDto) {
     return this.productGradeService.findMany(params);
   }
 
-  @Put('/productGrades/:gradeId')
+  @MessagePattern('edit-grade')
   updateGrade(
-    @Param('gradeId') gradeId: string,
-    @Body() dto: ProductGradeUpdateDto,
+    @Payload('id') id: number,
+    @Payload('dto') dto: ProductGradeUpdateDto,
   ) {
-    return this.productGradeService.update(+gradeId, dto);
+    return this.productGradeService.update(id, dto);
   }
 }
