5a8c23871d98fa431e36fe4717792e03edd3e7f3
Artyom Vorokhta <avorohta@newshift.ru>
2022-08-08 17:48:46 +0300

    lint fixes

--- src/migration/1659598752613-cascade.ts
+++ src/migration/1659598752613-cascade.ts
@@ -1,16 +1,17 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class cascade1659598752613 implements MigrationInterface {
-    name = 'cascade1659598752613'
+  name = 'cascade1659598752613';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "product" ADD "discount" integer NOT NULL DEFAULT '0'`);
-        await queryRunner.query(`ALTER TABLE "order" ADD "leadId" integer`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "product" ADD "discount" integer NOT NULL DEFAULT '0'`,
+    );
+    await queryRunner.query(`ALTER TABLE "order" ADD "leadId" integer`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "order" DROP COLUMN "leadId"`);
-        await queryRunner.query(`ALTER TABLE "product" DROP COLUMN "discount"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "order" DROP COLUMN "leadId"`);
+    await queryRunner.query(`ALTER TABLE "product" DROP COLUMN "discount"`);
+  }
 }
