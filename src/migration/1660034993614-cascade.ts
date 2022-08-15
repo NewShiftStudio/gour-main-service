b31c5f5c62571f604eb8f155907e07204c9b11e5
Artyom Vorokhta <avorohta@newshift.ru>
2022-08-15 12:18:26 +0300

    after lint

--- src/migration/1660034993614-cascade.ts
+++ src/migration/1660034993614-cascade.ts
@@ -1,14 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class cascade1660034993614 implements MigrationInterface {
-    name = 'cascade1660034993614'
+  name = 'cascade1660034993614';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "client" ADD "lives" integer NOT NULL DEFAULT '3'`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "client" ADD "lives" integer NOT NULL DEFAULT '3'`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "client" DROP COLUMN "lives"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "client" DROP COLUMN "lives"`);
+  }
 }
