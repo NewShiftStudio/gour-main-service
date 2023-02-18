d7d4e69f4dbfc0851c6f13e49cf91a0e780da64e
Enigmadie <gagen666@mail.ru>
2023-02-18 23:17:56 +0400

    Revert "Revert "add check start period in volume rendering""
    This reverts commit 49292ce08fbc64ef2977d8fa2d83936927367037.

--- src/features/referral-code/referral-code.service.ts
+++ src/features/referral-code/referral-code.service.ts
@@ -94,7 +94,7 @@ export class ReferralCodeService {
     if (!foundClients) throw new NotFoundException('Рефералы не найдены');
 
     const successPayments = await firstValueFrom(
-      this.client.send('success-payments', dto || {}),
+      this.client.send('success-payments', dto.start || {}),
     );
 
     const normalizedClients = foundClients.reduce((acc, client) => ({ ...acc, [client.id]: client }), {});
