71cf7f7d10bfcd4701b32a0d3c87b9fad3e40c0b
Enigmadie <gagen666@mail.ru>
2023-02-21 21:11:46 +0400

    add reserve for moysklad

--- src/features/warehouse/moysklad.service.ts
+++ src/features/warehouse/moysklad.service.ts
@@ -171,6 +171,7 @@ export class MoyskladService implements AbstractService {
       ),
     );
 
+    console.log('DATA STOCK', data);
     const reserve = data[0]?.reserve || 0;
 
     return { id: data[0]?.assortmentId, value: data[0]?.stock - reserve };
