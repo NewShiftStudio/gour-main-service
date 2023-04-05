119aa5a791b27e2a821e53c668a8d6920a9e5775
Warmic <kopylovpeter0@gmail.com>
2023-04-05 15:21:40 +0300

    create order fix

--- src/features/order/order.service.ts
+++ src/features/order/order.service.ts
@@ -247,13 +247,13 @@ export class OrderService {
       // );
 
       const assortment: AssortmentDto[] = orderWithTotalSum.orderProducts.map(
-        (p) => ({
+        (p) => (Object.assign({
           discount: 0,
           price: p.totalSumWithoutAmount * 100, // цена в копейках
           quantity: p.product.isWeighed ? (p.amount * p.gram / 1000) : p.amount, // вес либо количество
           type: p.product.isWeighed ? 'product' : 'variant',
           productId: p.product?.moyskladId,
-        }),
+        },p.product.isWeighed ? {} : {gram: p.gram})),
       );
 
       const fullClient = await this.clientService.findOne(client.id);
